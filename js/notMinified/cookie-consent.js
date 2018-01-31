bc04a4b2e75232bf22cbfc7fd63539d9157c8a5e
Axel Pettersson <axel@pettersson.cc>
2018-01-31 13:07:44 +0100

    Sitemap update and cache handling

---
+++ js/notMinified/cookie-consent.js
@@ -0,0 +1,31 @@
+    var gaProperty = 'UA-111642551-1';
+
+    var disableStr = 'ga-disable-' + gaProperty;
+
+    //Check if user has accepted cookies
+    if (document.cookie.indexOf("consent=true") == -1) {
+        jQuery(function ($) {
+            $('#cookie-banner').slideToggle();
+        });
+        window[disableStr] = true;
+    }
+    window.dataLayer = window.dataLayer || [];
+
+    function gtag() {
+        dataLayer.push(arguments);
+    }
+    gtag('js', new Date());
+    gtag('config', gaProperty);
+
+    //Enable Analytics
+    function gaOpt() {
+        jQuery(function ($) {
+            $('#cookie-banner').slideToggle();
+        });
+        document.cookie = "consent=true; expires=Thu, 31 Dec 2099 23:59:59 UTC; path=/";
+    }
+    function closeBanner(){
+        jQuery(function ($) {
+            $('#cookie-banner').slideToggle();
+        });
+    }
