5ed5afc9abde52358c08247def90b304b8ca0cb0
Axel Pettersson <axel@pettersson.cc>
2018-02-03 15:02:12 +0100

    Update cookie consent

--- js/notMinified/cookie-consent.js
+++ js/notMinified/cookie-consent.js
@@ -1,22 +1,9 @@
-//    var gaProperty = 'UA-111642551-1';
-
-//    var disableStr = 'ga-disable-' + gaProperty;
-
     //Check if user has accepted cookies
     if (document.cookie.indexOf("consent=true") == -1) {
         jQuery(function ($) {
             $('#cookie-banner').slideToggle();
         });
-       // window[disableStr] = true;
     }
-    //window.dataLayer = window.dataLayer || [];
-
-    //function gtag() {
-      //  dataLayer.push(arguments);
-    //}
-    //gtag('js', new Date());
-    //gtag('config', gaProperty);
-
     //Enable Analytics
     function gaOpt() {
         jQuery(function ($) {
