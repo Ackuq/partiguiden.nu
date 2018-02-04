1b4e935822276706d415b0d237571a40cb7c16a9
Axel Pettersson <axel@pettersson.cc>
2018-02-04 01:05:09 +0100

    Removed duplicates

--- js/cookie-consent.js
+++ js/cookie-consent.js
@@ -1,1 +1,18 @@
-function gaOpt(){jQuery(function(e){e("#cookie-banner").slideToggle()}),document.cookie="consent=true; expires=Thu, 31 Dec 2099 23:59:59 UTC; path=/"}function closeBanner(){jQuery(function(e){e("#cookie-banner").slideToggle()})}-1==document.cookie.indexOf("consent=true")&&jQuery(function(e){e("#cookie-banner").slideToggle()});
+    //Check if user has accepted cookies
+    if (document.cookie.indexOf("consent=true") == -1) {
+        jQuery(function ($) {
+            $('#cookie-banner').slideToggle();
+        });
+    }
+    //Enable Analytics
+    function gaOpt() {
+        jQuery(function ($) {
+            $('#cookie-banner').slideToggle();
+        });
+        document.cookie = "consent=true; expires=Thu, 31 Dec 2099 23:59:59 UTC; path=/";
+    }
+    function closeBanner(){
+        jQuery(function ($) {
+            $('#cookie-banner').slideToggle();
+        });
+    }
--- js/search.js
+++ js/search.js
@@ -1,1 +1,54 @@
-function displaySearchResults(e,t){var r=document.getElementById("search-results");if(e.length){for(var s="",a=0;a<e.length;a++){var i=t[e[a].ref];s+='<a href="'+i.url+'" ><span class="search-title">'+i.title+"</span></a>"}r.innerHTML=s}else r.innerHTML="<a>Inga resultat hittades</a>"}function getQueryVariable(e){for(var t=window.location.search.substring(1).split("&"),r=0;r<t.length;r++){var s=t[r].split("=");if(s[0]===e)return decodeURIComponent(s[1].replace(/\+/g,"%20"))}}var searchTerm=getQueryVariable("query");if(searchTerm){document.getElementById("search-box").setAttribute("value",searchTerm);var idx=lunr(function(){this.field("id"),this.field("title",{boost:10}),this.field("tags")});for(var key in window.store){idx.add({id:key,title:window.store[key].title,tags:window.store[key].tags});var results=idx.search(searchTerm);displaySearchResults(results,window.store)}}
+function displaySearchResults(results, store) {
+  var searchResults = document.getElementById('search-results');
+
+  if (results.length) { // Are there any results?
+    var appendString = '';
+
+    for (var i = 0; i < results.length; i++) {  // Iterate over the results
+      var item = store[results[i].ref];
+      appendString += '<a href="' + item.url + '" >'+ '<span class="search-title">' + item.title + '</span>' + '</a>';
+    }
+
+    searchResults.innerHTML = appendString;
+  } else {
+    searchResults.innerHTML = '<a>Inga resultat hittades</a>';
+  }
+}
+
+function getQueryVariable(variable) {
+  var query = window.location.search.substring(1);
+  var vars = query.split('&');
+
+  for (var i = 0; i < vars.length; i++) {
+    var pair = vars[i].split('=');
+
+    if (pair[0] === variable) {
+      return decodeURIComponent(pair[1].replace(/\+/g, '%20'));
+    }
+  }
+}
+
+var searchTerm = getQueryVariable('query');
+
+if (searchTerm) {
+  document.getElementById('search-box').setAttribute("value", searchTerm);
+
+  // Initalize lunr with the fields it will be searching on. I've given title
+  // a boost of 10 to indicate matches on this field are more important.
+  var idx = lunr(function () {
+    this.field('id');
+    this.field('title', { boost: 10 });
+    this.field('tags');
+  });
+
+  for (var key in window.store) { // Add the data to lunr
+    idx.add({
+      'id': key,
+      'title': window.store[key].title,
+      'tags': window.store[key].tags
+    });
+
+    var results = idx.search(searchTerm); // Get lunr to perform a search
+    displaySearchResults(results, window.store); // We'll write this in the next section
+  }
+}
