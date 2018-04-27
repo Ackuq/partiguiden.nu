eb0c4dd695ce0e37e69465b011860c16a839b769
Axel Pettersson <axel@pettersson.cc>
2018-04-27 13:01:09 +0200

    Minor fixes + cookie policy + removed kontaktsida

--- js/cookie-consent.js
+++ js/cookie-consent.js
@@ -1,18 +1,18 @@
     //Check if user has accepted cookies
-    if (document.cookie.indexOf("consent=true") == -1) {
-        jQuery(function ($) {
-            $('#cookie-banner').slideToggle();
-        });
-    }
-    //Enable Analytics
-    function gaOpt() {
-        jQuery(function ($) {
-            $('#cookie-banner').slideToggle();
-        });
-        document.cookie = "consent=true; expires=Thu, 31 Dec 2099 23:59:59 UTC; path=/";
-    }
-    function closeBanner(){
-        jQuery(function ($) {
-            $('#cookie-banner').slideToggle();
-        });
-    }
+if (document.cookie.indexOf("consent=true") == -1) {
+    jQuery(function ($) {
+        $('#cookie-banner').slideToggle();
+    });
+}
+//Enable Analytics
+function gaOpt() {
+    jQuery(function ($) {
+        $('#cookie-banner').slideToggle();
+    });
+    document.cookie = "consent=true; expires=Thu, 31 Dec 2099 23:59:59 UTC; path=/";
+}
+function closeBanner(){
+    jQuery(function ($) {
+        $('#cookie-banner').slideToggle();
+    });
+}
---
+++ kakor.md
@@ -0,0 +1,15 @@
+---
+layout: default
+title: Kakpolicy
+---
+<div id="content">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 offset-md-2">
+                <h1 class="text-center font-weight-light mt-30">{{page.title}}</h1>
+                <h4 class="contentText">Partiguiden.nu använder webbkakor från Google för att kunna analysera användares interaktion med hemsidan i syftet att kunna vidareutveckla sidan.<br><br>
+                Informationen som delas är krypterad och helt anonym, vi kan alltså inte identifera användare.</h4>
+            </div>
+        </div>
+    </div>
+</div>
